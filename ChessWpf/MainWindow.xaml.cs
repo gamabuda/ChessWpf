0ccaf7478f0610681423e68625e85a185309f887
MorFyNC <bulat.hisambeev@gmail.com>
2024-03-05 23:44:43 +0300

    Верстка поля

--- ChessWpf/MainWindow.xaml.cs
+++ ChessWpf/MainWindow.xaml.cs
@@ -23,6 +23,26 @@ namespace ChessWpf
         public MainWindow()
         {
             InitializeComponent();
+            for (int i = 0; i <= 8; i++)
+            {
+                RowDefinition row = new RowDefinition();
+                ColumnDefinition col = new ColumnDefinition();
+
+                Field.RowDefinitions.Add(row);
+                Field.ColumnDefinitions.Add(col);
+
+                for (int j = 0; j <= 8; j++)
+                {
+                    Button btn = new Button();
+                    btn.Background = i % 2 != j % 2 ? Brushes.Black : Brushes.White;
+                    btn.HorizontalContentAlignment = HorizontalAlignment.Left;
+                    btn.VerticalContentAlignment = VerticalAlignment.Top;
+
+                    Grid.SetRow(btn, j);
+                    Grid.SetColumn(btn, i);
+                    Field.Children.Add(btn);
+                }
+            }
         }
     }
 }
