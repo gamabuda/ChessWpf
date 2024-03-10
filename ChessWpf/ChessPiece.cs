65c983a79e1cc143d52c96ef1c3cba21f4646358
Камила Фаттахова <kamilafattahova65210@gmail.com>
2024-03-11 02:41:14 +0300

    the pawn is almost done

---
+++ ChessWpf/ChessPiece.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessWpf
+{
+    class ChessPiece
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public virtual bool Move(int newX, int newY)
+        {
+            return false;
+        }
+    }
+
+    class Pawn : ChessPiece
+    {
+        public Pawn(int startX, int startY)
+        {
+            x = startX;
+            y = startY;
+        }
+        public override bool Move(int newX, int newY)
+        {
+            int newwwX = newX - x;
+            int newwwY = newY - y;
+
+            if ((newwwX == 0 && newwwY == 1) || (newwwX == 0 && newwwY == 2))
+            {
+                Console.WriteLine("Пешка ходит на координаты ({0}, {1})", newX, newY);
+                x = newX;
+                y = newY;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Пешка не может сходить на данные координаты");
+                return false;
+            }
+        }
+    }
+
+    class King : ChessPiece
+    {
+        public King(int startX, int startY)
+        {
+            x = startX;
+            y = startY;
+        }
+        public override bool Move(int newX, int newY)
+        {
+            int newwwX = newX - x;
+            int newwwY = newY - y;
+
+            if (newwwX <= 1 && newwwY <= 1)
+            {
+                Console.WriteLine("Король ходит на координаты ({0}, {1})", newX, newY);
+                x = newX;
+                y = newY;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Король не может сходить на данные координаты");
+                return false;
+            }
+        }
+    }
+
+    class Ferz : ChessPiece
+    {
+        public Ferz(int startX, int startY)
+        {
+            x = startX;
+            y = startY;
+        }
+        public override bool Move(int newX, int newY)
+        {
+            int newwwX = newX - x;
+            int newwwY = newY - y;
+
+            if (newwwX == 0 || newwwY == 0 || newwwX == newwwY)
+            {
+                Console.WriteLine("Ферзь ходит на координаты ({0}, {1})", newX, newY);
+                x = newX;
+                y = newY;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Ферзь не может сходить на данные координаты");
+                return false;
+            }
+        }
+    }
+
+    class Ladiya : ChessPiece
+    {
+        public Ladiya(int startX, int startY)
+        {
+            x = startX;
+            y = startY;
+        }
+        public override bool Move(int newX, int newY)
+        {
+            int newwwX = newX - x;
+            int newwwY = newY - y;
+
+            if (newwwX == 0 || newwwY == 0)
+            {
+                Console.WriteLine("Ладья ходит на координаты ({0}, {1})", newX, newY);
+                x = newX;
+                y = newY;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Ладья не может сходить на данные координаты");
+                return false;
+            }
+        }
+    }
+
+    class Slon : ChessPiece
+    {
+        public Slon(int startX, int startY)
+        {
+            x = startX;
+            y = startY;
+        }
+        public override bool Move(int newX, int newY)
+        {
+            int newwwX = newX - x;
+            int newwwY = newY - y;
+
+            if (newwwX == newwwY)
+            {
+                Console.WriteLine("Слон ходит на координаты ({0}, {1})", newX, newY);
+                x = newX;
+                y = newY;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Слон не может сходить на данные координаты");
+                return false;
+            }
+        }
+    }
+
+    class Kon : ChessPiece
+    {
+        public Kon(int startX, int startY)
+        {
+            x = startX;
+            y = startY;
+        }
+        public override bool Move(int newX, int newY)
+        {
+            int newwwX = newX - x;
+            int newwwY = newY - y;
+
+            if ((newwwX == 2 && newwwY == 1) || (newwwX == 1 && newwwY == 2))
+            {
+                Console.WriteLine("Конь ходит на координаты ({0}, {1})", newX, newY);
+                x = newX;
+                y = newY;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Конь не может сходить на данные координаты");
+                return false;
+            }
+        }
+    }
+}
--- ChessWpf/MainWindow.xaml.cs
+++ ChessWpf/MainWindow.xaml.cs
@@ -15,23 +15,135 @@ using System.Windows.Shapes;
 
 namespace ChessWpf
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private ChessPiece selectedChessPiece;
         public MainWindow()
         {
             InitializeComponent();
+            InitializeChessboard();
+        }
 
-            Button button = new Button();
-            Grid grid = new Grid();
+        private void InitializeChessboard()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                AddChessPiece(new Pawn(i, 1), i, 1);
+                AddChessPiece(new Pawn(i, 6), i, 6);
+            }
+        }
+
+        private void AddChessPiece(ChessPiece chessPiece, int column, int row)
+        {
+            TextBlock textBlock = new TextBlock
+            {
+                Text = GetPictureByChessPiece(chessPiece),
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center
+            };
+            Grid.SetColumn(textBlock, chessPiece.x);
+            Grid.SetRow(textBlock, chessPiece.y);
+            textBlock.MouseLeftButtonDown += ChessPiece_Click;
+            ChessGrid.Children.Add(textBlock);
+        }
+
+        private string GetPictureByChessPiece(ChessPiece chessPiece)
+        {
+            switch (chessPiece)
+            {
+                case Pawn _:
+                    return "♙";
+                case King _:
+                    return "♔";
+                case Ferz _:
+                    return "♕";
+                case Ladiya _:
+                    return "♖";
+                case Slon _:
+                    return "♗";
+                case Kon _:
+                    return "♘";
+                default:
+                    return "";
+            }
+        }
+
+        private void ChessPiece_Click(object sender, MouseButtonEventArgs e)
+        {
+            var textBlock = sender as TextBlock;
+            int column = Grid.GetColumn(textBlock);
+            int row = Grid.GetRow(textBlock);
+            selectedChessPiece = GetChessPieceAtPosition(column, row);
+
+            if (selectedChessPiece != null)
+            {
+                Console.WriteLine($"Selected ChessPiece at ({column}, {row})");
+            }
+        }
+
+        private ChessPiece GetChessPieceAtPosition(int column, int row)
+        {
+            foreach (var chessPiece in ChessGrid.Children)
+            {
+                if (chessPiece is TextBlock textBlock && Grid.GetColumn(textBlock) == column && Grid.GetRow(textBlock) == row)
+                {
+                    return GetChessPieceByPicture(textBlock.Text, column, row);
+                }
+            }
+            return null;
+        }
+
+        private ChessPiece GetChessPieceByPicture(string unicode, int x, int y)
+        {
+            switch (unicode)
+            {
+                case "♙":
+                    return new Pawn(x, y);
+                default:
+                    return null;
+            }
+        }
+
+        private void UpdChessPiecePosition(ChessPiece chessPiece, int column, int row)
+        {
+            ChessGrid.Children.Remove(GetTextBlockAtPosition(chessPiece.x, chessPiece.y));
+            AddChessPiece(chessPiece, column, row);
+        }
+
+        private TextBlock GetTextBlockAtPosition(int x, int y)
+        {
+            foreach (var chessPiece in ChessGrid.Children)
+            {
+                if (chessPiece is TextBlock textBlock && Grid.GetColumn(textBlock) == x && Grid.GetRow(textBlock) == y)
+                {
+                    return textBlock;
+                }
+            }
+            return null;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var btn = sender as Button;
-            btn.Content = "Pawn";
+            var button = sender as Button;
+            int column = Grid.GetColumn(button);
+            int row = Grid.GetRow(button);
+
+            if (selectedChessPiece != null)
+            {
+                if (selectedChessPiece.Move(column, row))
+                {
+                    UpdChessPiecePosition(selectedChessPiece, column, row);
+                    selectedChessPiece = null;
+                }
+                else
+                {
+                    MessageBox.Show("Invalid move for the selected piece!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Select a piece by clicking on it before making a move.");
+            }
         }
     }
 }
