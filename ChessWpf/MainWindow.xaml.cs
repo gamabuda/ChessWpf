c29b3bca1d61e24ae436d788afe8da621838330c
polinabydugina <xch.victor@mail.ru>
2024-03-14 14:47:56 +0300

    komit

--- ChessWpf/MainWindow.xaml.cs
+++ ChessWpf/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ChessWpf;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,9 @@ namespace ChessWpf
         private readonly SolidColorBrush lightSquareBrush = new SolidColorBrush(Colors.Beige);
         private readonly SolidColorBrush darkSquareBrush = new SolidColorBrush(Colors.SaddleBrown);
         private const int boardSize = 8;
+        List<Button> buttons = new List<Button>();
+
+        Button ActiveSquare;
         public MainWindow()
         {
             InitializeComponent();
@@ -32,6 +37,7 @@ namespace ChessWpf
 
         private void InitializeBoard()
         {
+            Pawn pawn = new Pawn(ChessPiece.Color.White, new int[] { 7, 0 });
             for (int row = 0; row < boardSize; row++)
             {
                 for (int col = 0; col < boardSize; col++)
@@ -47,7 +53,15 @@ namespace ChessWpf
                     Grid.SetRow(square, row);
                     Grid.SetColumn(square, col);
 
+                    if(row == 7 && col == 0)
+                    {
+                        ChessSquare aa = square.Tag as ChessSquare;
+                        aa.ChessPiece = pawn;
+                        square.Content = pawn;
+                    }
+
                     chessBoard.Children.Add(square);
+                    buttons.Add(square);
                 }
             }
         }
@@ -56,7 +70,27 @@ namespace ChessWpf
             var button = (Button)sender;
             var square = (ChessSquare)button.Tag;
 
-            MessageBox.Show($"You clicked on square: {square.Row}, {square.Column}");
+            var activeSquare = ActiveSquare?.Tag as ChessSquare;
+
+            var movesqr = new int[] { square.Row, square.Column };
+
+            if (ActiveSquare is null)
+            {
+                ActiveSquare = button;
+                return;
+            }
+
+            if(activeSquare.ChessPiece.Move(movesqr))
+            {
+                activeSquare.ChessPiece.Position = movesqr;
+                square.ChessPiece = activeSquare.ChessPiece;
+                button.Content = square.ChessPiece;
+
+                activeSquare.ChessPiece = null;
+                ActiveSquare.Content = null;
+
+                ActiveSquare = null;
+            }
         }
     }
 }
@@ -64,7 +98,7 @@ public class ChessSquare
 {
     public int Row { get; }
     public int Column { get; }
-
+    public ChessPiece ChessPiece { get; set; }
     public ChessSquare(int row, int column)
     {
         Row = row;
---
+++ ChessWpf/Pawn.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessWpf
+{
+    public class Pawn : ChessPiece
+    {
+        public Pawn(Color color, int[] position) : base(color, position)
+        {
+        }
+
+        public override bool Move(int[] newPosition)
+        {
+            int oldRow = Position[0];
+            int newRow = newPosition[0];
+            int oldCol = Position[1];
+            int newCol = newPosition[1];
+
+            if (PieceColor == Color.White)
+            {
+                if (oldCol == newCol && oldRow - newRow == 1)
+                {
+                    isFirstMove = true;
+                    return true;
+                }
+                if(oldCol == newCol && oldRow - newRow == 2 && isFirstMove)
+                {
+                    isFirstMove = false;
+                    return true;
+                }
+                return false;
+            }
+            else
+            {
+                if (oldCol == newCol && oldRow - newRow == -1)
+                {
+                    isFirstMove = true;
+                    return true;
+                }
+                if (oldCol == newCol && oldRow - oldRow == -2 && isFirstMove)
+                {
+                    isFirstMove = true;
+                    return true;
+                }
+                return false;
+            }
+        }
+        public override string ToString()
+        {
+            return "Pawn";
+        }
+    }
+}
