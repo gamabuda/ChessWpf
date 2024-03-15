70dd700cbf837057afc8807ac647e2293d647982
Богдан Попов <bogdanpopov5570@gmail.com>
2024-03-16 01:31:28 +0300

    люблбю\\ айгизу живи родная

---
+++ ChessLib/Pawn.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLib
+{
+    public class Pawn
+    {
+        public int position;
+        public bool isWhite;
+
+        public Pawn(int position, bool isWhite)
+        {
+            this.position = position;
+            this.isWhite = isWhite;
+        }
+
+        public bool CanMove(int newPosition)
+        {
+            if (newPosition < 1 || newPosition > 8)
+                return false;
+
+            if (isWhite)
+            {
+                if (position == newPosition)
+                    return false;
+
+                if (position == 2 && newPosition == 4)
+                    return true;
+
+                if (position == 2 && newPosition == 3)
+                    return true;
+
+                if (position == 3 && newPosition == 4)
+                    return true;
+
+                if (position > 2 && newPosition == position + 1)
+                    return true;
+
+                if (position == 7 && newPosition == 8)
+                    return true;
+
+                return false;
+            }
+            else
+            {
+                if (position == newPosition)
+                    return false;
+
+                if (position == 7 && newPosition == 5)
+                    return true;
+
+                if (position == 7 && newPosition == 6)
+                    return true;
+
+                if (position == 6 && newPosition == 5)
+                    return true;
+
+                if (position < 7 && newPosition == position - 1)
+                    return true;
+
+                if (position == 2 && newPosition == 1)
+                    return true;
+
+                return false;
+
+            }
+        }
+    }
+}
--- ChessWpf/MainWindow.xaml.cs
+++ ChessWpf/MainWindow.xaml.cs
@@ -1,7 +1,4 @@
-﻿using ChessLib.Boards;
-using ChessLib.Figures;
-using ChessLib.Interfaces;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,242 +12,126 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ChessLib;
 
 namespace ChessWpf
 {
     public partial class MainWindow : Window
     {
-        IFigure CurrentFigure { get; set; }
-        Button PrevPress { get; set; }
-        Button[,] Board { get; set; } = new Button[10, 10];
+        private Dictionary<int, Pawn> pawns = new Dictionary<int, Pawn>();
         public MainWindow()
         {
             InitializeComponent();
-            InitializeBoard();
-        }
-
-        private void InitializeBoard()
-        {
-            for (int row = 1; row < 9; row++)
+            for (int i = 0; i < 8; i++)
             {
-                for (int col = 1; col < 9; col++)
+                for (int j = 0; j < 8; j++)
                 {
-                    var square = new Button
+                    Button button = new Button();
+                    Pawn pawn = new Pawn(i, true);
+                    button.Width = 85;
+                    button.Height = 95;
+
+                    if ((i + j) % 2 == 0)
                     {
-                        Background = (row + col) % 2 == 0 ? Brushes.SaddleBrown : (Brush)new BrushConverter().ConvertFrom("#D6B994"),
-                        Tag = new Cell(row, col),
-                        BorderBrush = Brushes.Transparent,
-                        FontSize = 36,
-                        Width = 70,
-                        Height = 70
-                    };
+                        button.Background = Brushes.Black;
+                    }
+                    else
+                    {
+                        button.Background = Brushes.White;
+                    }
 
-                    if (row == 1 || row == 2)
-                        ArrangeFigures(square, "black", row, col);
-                    else if (row == 7 || row == 8)
-                        ArrangeFigures(square, "white", row, col);
+                    Grid.SetRow(button, i);
+                    Grid.SetColumn(button, j);
+                    boardGrid.Children.Add(button);
+                    button.Click += Button_Click;
+                    pawns[i] = pawn;
 
-                    Grid.SetRow(square, row);
-                    Grid.SetColumn(square, col);
-                    grid.Children.Add(square);
-
-                    square.Click += Button_Click;
-                    Board[row, col] = square;
+                    TextBlock textBlock = new TextBlock();
+                    textBlock.Text = "pawn";
+                    textBlock.Foreground = Brushes.Black;
+                    Grid.SetRow(textBlock, i);
+                    Grid.SetColumn(textBlock, j);
+                    boardGrid.Children.Add(textBlock);
                 }
             }
 
-            char letter = 'A';
-
-            for (int i = 1; i < 9; i++)
+            for (int i = 0; i < 8; i++)
             {
-                Label label = new Label();
+                Button button = new Button();
+                Pawn pawn = new Pawn(i, false);
+                button.Width = 85;
+                button.Height = 95;
 
-                label.Width = 70;
-                label.Height = 70;
-                label.Foreground = Brushes.DimGray;
-                label.Content = letter;
-                label.HorizontalContentAlignment = HorizontalAlignment.Center;
-                label.VerticalContentAlignment = VerticalAlignment.Center;
-                label.FontSize = 30;
-
-                Grid.SetRow(label, 0);
-                Grid.SetColumn(label, i);
-                grid.Children.Add(label);
-
-                Label label2 = new Label();
-
-                label2.Width = 70;
-                label2.Height = 70;
-                label2.Foreground = Brushes.DimGray;
-                label2.Content = letter;
-                label2.HorizontalContentAlignment = HorizontalAlignment.Center;
-                label2.VerticalContentAlignment = VerticalAlignment.Center;
-                label2.FontSize = 30;
-
-                Grid.SetRow(label2, 9);
-                Grid.SetColumn(label2, i);
-                grid.Children.Add(label2);
-
-                letter++;
-            }
-
-            for (int j = 1; j < 9; j++)
-            {
-                Label label = new Label();
-
-                label.Width = 70;
-                label.Height = 70;
-                label.Foreground = Brushes.DimGray;
-                label.Content = j;
-                label.HorizontalContentAlignment = HorizontalAlignment.Center;
-                label.VerticalContentAlignment = VerticalAlignment.Center;
-                label.FontSize = 30;
-
-                Grid.SetRow(label, j);
-                Grid.SetColumn(label, 0);
-                grid.Children.Add(label);
-
-                Label label2 = new Label();
-
-                label2.Width = 70;
-                label2.Height = 70;
-                label2.Foreground = Brushes.DimGray;
-                label2.Content = j;
-                label2.HorizontalContentAlignment = HorizontalAlignment.Center;
-                label2.VerticalContentAlignment = VerticalAlignment.Center;
-                label2.FontSize = 30;
-
-                Grid.SetRow(label2, j);
-                Grid.SetColumn(label2, 9);
-                grid.Children.Add(label2);
-            }
-        }
-
-        private void ArrangeFigures(object sender, string playerColor, int i, int j)
-        {
-            var button = (Button)sender;
-            var square = (Cell)button.Tag;
-
-            if (playerColor.ToLower().Trim() == "black")
-            {
-                button.Foreground = Brushes.Black;
-                button.FontWeight = FontWeights.ExtraBold;
-
-                if (i == 1 && j == 1 || i == 1 && j == 8)
-                    button.Content = "♖";
-                else if (i == 1 && j == 2 || i == 1 && j == 7)
-                    button.Content = "♘";
-                else if (i == 1 && j == 3 || i == 1 && j == 6)
-                    button.Content = "♗";
-                else if (i == 1 && j == 4)
-                    button.Content = "♕";
-                else if (i == 1 && j == 5)
-                    button.Content = "♔";
-                else if (i == 2)
+                if ((i + 1) % 2 == 0)
                 {
-                    Pawn pawn = new Pawn(square, "black");
-                    button.Content = pawn.Figure;
-                    square.Figure = pawn;
-                    square.Figure.Position.Row = i;
-                    square.Figure.Position.Column = j;
-                    square.isFilled = true;
-                    square.Figure.Color = "black";
+                    button.Background = Brushes.Black;
                 }
-
-            }
-            else if (playerColor.ToLower().Trim() == "white")
-            {
-                button.Foreground = Brushes.White;
-                button.FontWeight = FontWeights.Light;
-
-                if (i == 8 && j == 1 || i == 8 && j == 8)
-                    button.Content = "♖";
-                else if (i == 8 && j == 2 || i == 8 && j == 7)
-                    button.Content = "♘";
-                else if (i == 8 && j == 3 || i == 8 && j == 6)
-                    button.Content = "♗";
-                else if (i == 8 && j == 4)
-                    button.Content = "♕";
-                else if (i == 8 && j == 5)
-                    button.Content = "♔";
-                else if (i == 7)
+                else
                 {
-                    Pawn pawn = new Pawn(square, "white");
-                    button.Content = pawn.Figure;
-                    square.Figure = pawn;
-                    square.Figure.Position.Row = i;
-                    square.Figure.Position.Column = j;
-                    square.isFilled = true;
-                    square.Figure.Color = "white";
+                    button.Background = Brushes.White;
                 }
+
+                Grid.SetRow(button, 1);
+                Grid.SetColumn(button, i);
+                boardGrid.Children.Add(button);
+                button.Click += Button_Click;
+                pawns[i] = pawn;
+
+                TextBlock textBlock = new TextBlock();
+                textBlock.Text = "pawn";
+                textBlock.Foreground = Brushes.Black;
+                Grid.SetRow(textBlock, 1);
+                Grid.SetColumn(textBlock, i);
+                boardGrid.Children.Add(textBlock);
             }
-            else
-                throw new Exception("Incorrect player's color");
+
+            for (int i = 0; i < 8; i++)
+            {
+                Button button = new Button();
+                Pawn pawn = new Pawn(i, true);
+                button.Width = 85;
+                button.Height = 95;
+
+                if ((i + 1) % 2 == 0)
+                {
+                    button.Background = Brushes.Black;
+                }
+                else
+                {
+                    button.Background = Brushes.White;
+                }
+
+                Grid.SetRow(button, 6);
+                Grid.SetColumn(button, i);
+                boardGrid.Children.Add(button);
+                button.Click += Button_Click;
+                pawns[i] = pawn;
+
+                TextBlock textBlock = new TextBlock();
+                textBlock.Text = "pawn";
+                textBlock.Foreground = Brushes.Black;
+                Grid.SetRow(textBlock, 6);
+                Grid.SetColumn(textBlock, i);
+                boardGrid.Children.Add(textBlock);
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var button = (Button)sender;
-            var square = (Cell)button.Tag;
+            Button clickedButton = (Button)sender;
+            int row = Grid.GetRow(clickedButton);
+            Pawn pawn = pawns[row];
+            int newPosition = Grid.GetColumn(clickedButton);
 
-            //MessageBox.Show($"You clicked on figure: {square.Figure}");
-
-            //button.Content = null;
-            if (!square.isFilled && CurrentFigure != null)
+            if (pawn.CanMove(newPosition))
             {
-                bool moved = Move(button);
-                PrevPress.Content = moved ? ' ' : CurrentFigure.Figure;
-
-                if (!moved)
-                    MessageBox.Show($"Ход невозможен!");
-                square.Figure = CurrentFigure;
-                square.isFilled = true;
-                CurrentFigure = null;
-
-                foreach (var item in Board)
-                {
-                    if (item != null)
-                    {
-                        Cell cell = (Cell)item.Tag;
-                        item.Background = (cell.Row + cell.Column) % 2 == 0 ? Brushes.SaddleBrown : (Brush)new BrushConverter().ConvertFrom("#D6B994");
-                    }
-                }
+                pawn.position = newPosition;
+                MessageBox.Show("Ход успешно выполнен!");
             }
-            else if (square.isFilled)
+            else
             {
-                PrevPress = button;
-                CurrentFigure = square.Figure;
-                CurrentFigure.Position.Row = square.Row;
-                CurrentFigure.Position.Column = square.Column;
-
-                List<(int, int)> moves = CurrentFigure.CalculateAvailableMoves();
-                foreach (var move in moves)
-                {
-                    foreach (var item in Board)
-                    {
-                        if (item != null)
-                        {
-                            Cell cell = (Cell)item.Tag;
-                            if (cell.Row == move.Item1 && cell.Column == move.Item2)
-                                item.Background = Brushes.LightGreen;
-                        }
-                    }
-                }
+                MessageBox.Show("Невозможно сделать ход!");
             }
-            else if (CurrentFigure == null)
-                MessageBox.Show($"Сначала выберите фигуру!");
-        }
-
-        private bool Move(Button button)
-        {
-            var square = (Cell)button.Tag;
-
-            bool moved = CurrentFigure.Move(square);
-            button.Content = moved ? CurrentFigure.Figure : null;
-            button.FontWeight = CurrentFigure.Color == "black" ? FontWeights.Bold : FontWeights.Light;
-            button.Foreground = CurrentFigure.Color == "black" ? Brushes.Black : Brushes.White;
-
-            return moved;
         }
 
     }
