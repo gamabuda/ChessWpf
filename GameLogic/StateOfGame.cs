ca45b189c0d5aa1ea8188a510383c4b955bc2219
Всеволод Емельянов <eddy.weston@bk.ru>
2024-03-15 22:24:44 +0300

    Забыл докинуть класс :\

---
+++ GameLogic/StateOfGame.cs
@@ -0,0 +1,14 @@
+﻿namespace GameLogic
+{
+    public class StateOfGame
+    {
+        public GameField GameField { get; }
+        public Player CurrentPlayer { get; private set; }
+
+        public StateOfGame(Player player, GameField gameField)
+        {
+            CurrentPlayer = player;
+            GameField = gameField;
+        }
+    }
+}
